d0a12cc5761daadd69886640dcb8ba3ff7dbca3e
Kittiwut Khongkaeo <85986506+hereismeaw@users.noreply.github.com>
2022-02-13 13:28:53 +0700

    Using with Google Sheet Console Integration

--- decoder-ggsheet.js
+++ decoder-ggsheet.js
@@ -1,27 +1,13 @@
-function Decoder(bytes, port) {
+//Add on the before google sheet f(x)
 
-  var decodedPayload = {
-    "dust": bytes[0] << 8 | bytes[1]
-  };
+function Decoder(bytes, port) 
+{
 
+  var decodedPayload = 
+
+      {
+        "dust": bytes[0] << 8 | bytes[1]
+      };
 
   return Serialize(decodedPayload)
-}
-
-// do not touch unless your Google Form fields have changed
-var field_mapping = {
-  "dust": "entry.589685897"
-};
-
-
-function Serialize(payload) {
-  var str = [];
-  for (var key in payload) {
-    if (payload.hasOwnProperty(key)) {
-      var name = encodeURIComponent(field_mapping[key]);
-      var value = encodeURIComponent(payload[key]);
-      str.push(name + "=" + value);
-    }
-  }
-  return str.join("&");
 }
